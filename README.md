c46abf4c2957d24def20e75a1b4e082277a68ff5
Agustín Seguí <agugalgui@gmail.com>
2022-09-15 20:38:52 +0200

    tidying up basics

---
+++ README.md
@@ -0,0 +1,13 @@
+# vithree
+
+Just a simple [three.js](https://github.com/mrdoob/three.js) starter on Vite.
+Shader ready with glsl Vite plugin.
+Tweakpane ready for debugging.
+Gsap for motion.
+
+## install
+
+```bash
+npm i
+npm run dev
+```
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import './style.css'
+import "./style.css";
 
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 
-import { Pane } from 'tweakpane';
+import {Pane} from "tweakpane";
 import gsap from "gsap";
 
 import fragment from "./shaders/fragment.glsl";
@@ -42,11 +42,7 @@ export default class Sketch {
       1000
     );
 
-
-
     this.camera.position.set(0, 0, 1.3);
-
-  
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
@@ -56,43 +52,39 @@ export default class Sketch {
     THINGS
     */
 
-    this.clock = new THREE.Clock()
-    this.uMouse = new THREE.Vector2(0, 0)
-    this.prevMouse = new THREE.Vector2(0, 0)
+    this.clock = new THREE.Clock();
+    this.uMouse = new THREE.Vector2(0, 0);
+    this.prevMouse = new THREE.Vector2(0, 0);
 
-    this.settings()
-    this.addObjects()
+    this.settings();
+    this.addObjects();
 
-    this.resize()
-    this.setupResize()
-    this.mouseMovement()
-    this.render()
+    this.resize();
+    this.setupResize();
+    this.mouseMovement();
+    this.render();
   }
-
 
   cubeTarget() {
     this.cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, {
       format: THREE.RGBAFormat,
       generateMipmaps: true,
       minFilter: THREE.LinearMipMapLinearFilter,
-      encoding: THREE.sRGBEncoding
+      encoding: THREE.sRGBEncoding,
+    });
 
-    })
-
-    this.cubeCamera = new THREE.CubeCamera(0.1, 10, this.cubeRenderTarget)
+    this.cubeCamera = new THREE.CubeCamera(0.1, 10, this.cubeRenderTarget);
   }
 
   settings() {
     const pane = new Pane();
 
     this.parameters = {
-      uProgress:0,
-     
+      uProgress: 0,
     };
 
-    const f1 = pane.addFolder({title: 'Basic'});
-    f1.addInput(this.parameters, 'uProgress' , { min: 0, max: 1, step: 0.01 });
-    
+    const f1 = pane.addFolder({title: "Basic"});
+    f1.addInput(this.parameters, "uProgress", {min: 0, max: 1, step: 0.01});
   }
 
   setupResize() {
@@ -128,16 +120,13 @@ export default class Sketch {
   }
 
   addObjects() {
-
-  
-
-    this.geometry = new THREE.PlaneBufferGeometry(1,1,1,1);
+    this.geometry = new THREE.PlaneBufferGeometry(1, 1, 1, 1);
 
     this.material = new THREE.ShaderMaterial({
       uniforms: {
         uTime: {value: 0},
         uMouse: {value: new THREE.Vector2(0, 0)},
-        uProgress: {value: 0},  
+        uProgress: {value: 0},
         uTexture1: {value: new THREE.TextureLoader().load(fox1)},
         uTexture2: {value: new THREE.TextureLoader().load(fox2)},
         uResolution: {value: new THREE.Vector4()},
@@ -148,15 +137,9 @@ export default class Sketch {
       wireframe: false,
     });
 
-
-
-
     this.plane = new THREE.Mesh(this.geometry, this.material);
-   
 
-    this.scene.add(
-      this.plane,
-    );
+    this.scene.add(this.plane);
   }
 
   mouseMovement() {
@@ -172,29 +155,29 @@ export default class Sketch {
 
     window.addEventListener("mousedown", (e) => {
       gsap.to(this.parameters, {
-        duration: .5,
+        duration: 0.5,
         uProgress: 1,
-        ease: 'power3.out'  
-      })
+        ease: "power3.out",
+      });
 
-      if(this.parameters.uProgress === 1) {
+      if (this.parameters.uProgress === 1) {
         gsap.to(this.parameters, {
-        duration: .5,
-        uProgress: 0,
-        ease: 'power3.out'  
-      })
+          duration: 0.5,
+          uProgress: 0,
+          ease: "power3.out",
+        });
       }
 
       // console.log('down');
       // this.material.uniforms.uProgress.value = 1
-
-    })
+    });
   }
 
   render() {
     this.elapsedTime = this.clock.getElapsedTime();
 
     this.material.uniforms.uTime.value = this.elapsedTime;
+
     this.controls.update();
 
     this.renderer.render(this.scene, this.camera);
